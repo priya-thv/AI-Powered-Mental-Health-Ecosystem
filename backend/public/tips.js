d16a20d6b2c6e943f02c977a8639cfeff8680494
Angela161 <107641490+Angela161@users.noreply.github.com>
2025-05-24 23:59:53 +0530

    Create tips.js

---
+++ backend/public/tips.js
@@ -0,0 +1,8 @@
+ 
+        function goBack() {
+            window.location.href="well being hub.html";
+        }
+        document.getElementById("motivation-btn").addEventListener("click", function() {
+    window.location.href = "motivational.html"; // Redirect to challenges page
+});
+    
