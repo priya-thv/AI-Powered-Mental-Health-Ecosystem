d734a22d53429c6013f3a6e39cdf505fac9c7df6
priya <107272239+priya-thv@users.noreply.github.com>
2025-05-29 23:08:59 +0530

    Update signupjs.js

--- backend/public/signupjs.js
+++ backend/public/signupjs.js
@@ -1,3 +1,41 @@
+// import { apiFetch } from './helpers.js';
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   const signupForm = document.getElementById('signupForm');
+
+//   signupForm.addEventListener('submit', async (e) => {
+//     e.preventDefault();
+
+//     const fullname = document.getElementById('fullname').value.trim();
+//     const email = document.getElementById('email').value.trim();
+//     const password = document.getElementById('password').value.trim();
+//     const confirmpassword = document.getElementById('confirmpassword').value.trim();
+
+//     try {
+//       const response = await apiFetch('/api/auth/signup', {
+//         method: 'POST',
+//         headers: { 'Content-Type': 'application/json' },
+//         body: JSON.stringify({ fullname, email, password,confirmpassword }),
+//       });
+
+//       const data = await response.json();
+//       console.log(data);
+
+//       if (response.ok) {
+//         alert('Signup successful! Redirecting to login...');
+//         window.location.href = '/login.html';
+//       } else {
+//         alert(data.message || 'Signup failed!');
+//       }
+//     } catch (error) {
+//       console.error('Signup Error:', error);
+//       alert('Signup failed! Please try again.');
+//     }
+//   });
+// });
+
+
+// public/signup.js
 import { apiFetch } from './helpers.js';
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -15,7 +53,7 @@ document.addEventListener('DOMContentLoaded', () => {
       const response = await apiFetch('/api/auth/signup', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ fullname, email, password,confirmpassword }),
+        body: JSON.stringify({ fullname, email, password, confirmpassword }),
       });
 
       const data = await response.json();
